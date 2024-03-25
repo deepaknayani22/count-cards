fbf8ec48083241159cb28fd95cdbdf2eae51071f
deepaknayani22 <nayanideepakreddy@gmail.com>
2024-03-24 17:43:45 -0700

    Added homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import CardCounter from "./CardCounter";
 import "./styles.css";
 
+// Placeholder component for Basic Strategy
+function BasicStrategy() {
+  return <div className="container">Basic Strategy Page</div>;
+}
+
 function App() {
   return (
-    <div className="container">
-      <CardCounter />
-    </div>
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <h1>Welcome to the Black Jack ♠️ Trainer ♥️</h1>
+                <div className="home-buttons">
+                  <Link to="/count-cards">
+                    <button>Count Cards</button>
+                  </Link>
+                  <Link to="/basic-strategy">
+                    <button>Basic Strategy</button>
+                  </Link>
+                </div>
+              </div>
+            }
+          />
+          <Route path="/count-cards" element={<CardCounter />} />
+          <Route path="/basic-strategy" element={<BasicStrategy />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
